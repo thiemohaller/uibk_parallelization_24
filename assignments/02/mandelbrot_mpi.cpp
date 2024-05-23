c6d676721574f6774da4eb20829eee4a5db0c249
LV 703078 PS Parallele Programmierung <cb761148@login.lcc3.uibk.ac.at>
2024-05-23 15:55:18 +0200

    different approach, now dividing in main based on rank instead of in calcManedlbrot

--- assignments/02/mandelbrot_mpi.cpp
+++ assignments/02/mandelbrot_mpi.cpp
@@ -1,12 +1,12 @@
+#include <mpi.h>
 #include <bits/chrono.h>
-#include <chrono>
 #include <cstdint>
 #include <cstdlib>
 #include <iostream>
+#include <chrono>
 #include <sys/time.h>
 #include <tuple>
 #include <vector>
-#include "mpi.h"
 
 // Include that allows to print result as an image
 // Also, ignore some warnings that pop up when compiling this as C++ mode
@@ -15,7 +15,7 @@
 #pragma GCC diagnostic ignored "-Wstringop-overflow"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
-#pragma GCC diagnostic push
+#pragma GCC diagnostic pop
 
 constexpr int default_size_x = 1344;
 constexpr int default_size_y = 768;
@@ -27,139 +27,129 @@ constexpr int max_iterations = 10000;
 
 #define IND(Y, X, SIZE_Y, SIZE_X, CHANNEL) (Y * SIZE_X * num_channels + X * num_channels + CHANNEL)
 
-size_t index(int y, int x, int /*size_y*/, int size_x, int channel) { return y * size_x * num_channels + x * num_channels + channel; }
+size_t index(int y, int x, int /*size_y*/, int size_x, int channel) {
+    return y * size_x * num_channels + x * num_channels + channel;
+}
 
 using Image = std::vector<uint8_t>;
 
 auto HSVToRGB(double H, const double S, double V) {
-	if (H >= 1.0) {
-		V = 0.0;
-		H = 0.0;
-	}
+    if (H >= 1.0) {
+        V = 0.0;
+        H = 0.0;
+    }
 
-	const double step = 1.0 / 6.0;
-	const double vh = H / step;
+    const double step = 1.0 / 6.0;
+    const double vh = H / step;
 
-	const int i = (int)floor(vh);
+    const int i = (int)floor(vh);
 
-	const double f = vh - i;
-	const double p = V * (1.0 - S);
-	const double q = V * (1.0 - (S * f));
-	const double t = V * (1.0 - (S * (1.0 - f)));
-	double R = 0.0;
-	double G = 0.0;
-	double B = 0.0;
+    const double f = vh - i;
+    const double p = V * (1.0 - S);
+    const double q = V * (1.0 - (S * f));
+    const double t = V * (1.0 - (S * (1.0 - f)));
+    double R = 0.0;
+    double G = 0.0;
+    double B = 0.0;
 
-	// clang-format off
-	switch (i) {
-	case 0: { R = V; G = t; B = p; break; }
-	case 1: { R = q; G = V; B = p; break; }
-	case 2: { R = p; G = V; B = t; break; }
-	case 3: { R = p; G = q; B = V; break; }
-	case 4: { R = t; G = p; B = V; break; }
-	case 5: { R = V; G = p; B = q; break; }
-	}
-	// clang-format on
+    switch (i) {
+    case 0: { R = V; G = t; B = p; break; }
+    case 1: { R = q; G = V; B = p; break; }
+    case 2: { R = p; G = V; B = t; break; }
+    case 3: { R = p; G = q; B = V; break; }
+    case 4: { R = t; G = p; B = V; break; }
+    case 5: { R = V; G = p; B = q; break; }
+    }
 
-	return std::make_tuple(R, G, B);
+    return std::make_tuple(R, G, B);
 }
 
-void caclMandelbrotRow(Image &image, int size_x, int size_y, int start_row, int end_row, float left, float right, float bottom, float top) {
-	for (int pixel_y = start_row; pixel_y < end_row; pixel_y++) {
-		// scale y pixel into mandelbrot coordinate system
-		const float cy = (pixel_y / (float)size_y) * (top - bottom) + bottom;
-		for (int pixel_x = 0; pixel_x < size_x; pixel_x++) {
-			// scale x pixel into mandelbrot coordinate system
-			const float cx = (pixel_x / (float)size_x) * (right - left) + left;
-			float x = 0;
-			float y = 0;
-			int num_iterations = 0;
+void calcMandelbrot(Image &image, int size_x, int size_y, int start_y, int num_rows, int rank) {
 
-			// Check if the distance from the origin becomes
-			// greater than 2 within the max number of iterations.
-			while ((x * x + y * y <= 2 * 2) && (num_iterations < max_iterations)) {
-				float x_tmp = x * x - y * y + cx;
-				y = 2 * x * y + cy;
-				x = x_tmp;
-				num_iterations += 1;
-			}
+	std::cout << "Rank " << rank << " is calculating " << num_rows << " rows starting from " << start_y << " until " << start_y + size_y << std::endl;
 
-			// Normalize iteration and write it to pixel position
-			double value = fabs((num_iterations / (float)max_iterations)) * 200;
+    const float left = -2.5, right = 1;
+    const float bottom = -1, top = 1;
 
-			auto [red, green, blue] = HSVToRGB(value, 1.0, 1.0);
+    for (int pixel_y = start_y; pixel_y < start_y + num_rows; pixel_y++) {
+        const float cy = (pixel_y / (float)size_y) * (top - bottom) + bottom;
+        for (int pixel_x = 0; pixel_x < size_x; pixel_x++) {
+            const float cx = (pixel_x / (float)size_x) * (right - left) + left;
+            float x = 0;
+            float y = 0;
+            int num_iterations = 0;
 
-			int channel = 0;
-			image[index(pixel_y, pixel_x, size_y, size_x, channel++)] = (uint8_t)(red * UINT8_MAX);
-			image[index(pixel_y, pixel_x, size_y, size_x, channel++)] = (uint8_t)(green * UINT8_MAX);
-			image[index(pixel_y, pixel_x, size_y, size_x, channel++)] = (uint8_t)(blue * UINT8_MAX);
-		}
-	}
-}
+            while ((x * x + y * y <= 2 * 2) && (num_iterations < max_iterations)) {
+                float x_tmp = x * x - y * y + cx;
+                y = 2 * x * y + cy;
+                x = x_tmp;
+                num_iterations += 1;
+            }
 
-void calcMandelbrot(Image &image, int size_x, int size_y, int rank, int num_ranks) {
-	auto time_start = std::chrono::high_resolution_clock::now();
+            double value = fabs((num_iterations / (float)max_iterations)) * 200;
+            auto [red, green, blue] = HSVToRGB(value, 1.0, 1.0);
 
-	const float left = -2.5, right = 1;
-	const float bottom = -1, top = 1;
-
-	// TODOs for MPI parallelization
-	// 1) domain decomposition
-	//   - decide how to split the image into multiple parts
-	//   - ensure every rank is computing its own part only
-	// 2) result aggregation
-	//   - aggregate the individual parts of the ranks into a single, complete image on the root rank (rank 0)
-
-	// Determine the number of rows each rank will compute
-	int rows_per_rank = size_y / num_ranks;
-	// Determine the starting and ending rows for the current rank
-	int start_row = rank * rows_per_rank;
-	int end_row = start_row + rows_per_rank;
-
-	caclMandelbrotRow(image, size_x, size_y, start_row, end_row, left, right, bottom, top);
-	// Gather all the partial images from each rank to the root rank
-	std::vector<uint8_t> gathered_image;
-	if (rank == 0) {
-		gathered_image.resize(num_ranks * size_x * size_y * num_channels);
-	}
-	MPI_Gather(image.data(), image.size(), MPI_BYTE, gathered_image.data(), image.size(), MPI_UNSIGNED_CHAR, 0, MPI_COMM_WORLD);
-
-	// Save the gathered image on the root rank
-	if (rank == 0) {
-		constexpr int stride_bytes = 0;
-		stbi_write_png("mandelbrot_mpi.png", size_x, size_y * num_ranks, num_channels, gathered_image.data(), stride_bytes);
-	}
-
-	auto time_end = std::chrono::high_resolution_clock::now();
-	auto time_elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(time_end - time_start).count();
-
-	std::cout << "Mandelbrot set calculation for " << size_x << "x" << size_y << " took: " << time_elapsed << " ms." << std::endl;
+            int channel = 0;
+            image[index(pixel_y - start_y, pixel_x, size_y, size_x, channel++)] = (uint8_t)(red * UINT8_MAX);
+            image[index(pixel_y - start_y, pixel_x, size_y, size_x, channel++)] = (uint8_t)(green * UINT8_MAX);
+            image[index(pixel_y - start_y, pixel_x, size_y, size_x, channel++)] = (uint8_t)(blue * UINT8_MAX);
+        }
+    }
 }
 
 int main(int argc, char **argv) {
-	MPI_Init(&argc, &argv);
-	int rank, num_ranks;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	MPI_Comm_size(MPI_COMM_WORLD, &num_ranks);
-	
-	int size_x = default_size_x;
-	int size_y = default_size_y;
+	// mpi stuff
+    MPI_Init(&argc, &argv);
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-	if (argc == 3) {
-		size_x = atoi(argv[1]);
-		size_y = atoi(argv[2]);
-		std::cout << "Using size " << size_x << "x" << size_y << std::endl;
-	} else {
-		std::cout << "No arguments given, using default size " << size_x << "x" << size_y << std::endl;
-	}
+    int size_x = default_size_x;
+    int size_y = default_size_y;
 
-	Image image(num_channels * size_x * size_y);
+    if (argc == 3) {
+        size_x = atoi(argv[1]);
+        size_y = atoi(argv[2]);
+        if (rank == 0) {
+            std::cout << "Using size " << size_x << "x" << size_y << std::endl;
+        }
+    } else {
+        if (rank == 0) {
+            std::cout << "No arguments given, using default size " << size_x << "x" << size_y << std::endl;
+        }
+    }
 
-	calcMandelbrot(image, size_x, size_y, 0, 5);
+	// -----
+    int num_rows_per_rank = size_y / size;
+    int start_y = rank * num_rows_per_rank; // slice based on rank
+    int num_rows = (rank == size - 1) ? (size_y - start_y) : num_rows_per_rank; // handle remainder if y is not divisible by size
 
-	constexpr int stride_bytes = 0;
-	stbi_write_png("mandelbrot_mpi.png", size_x, size_y, num_channels, image.data(), stride_bytes);
-	MPI_Finalize();
+	int sub_image_size = num_channels * size_x * num_rows;
+    Image sub_image(sub_image_size);
+    auto time_start = std::chrono::high_resolution_clock::now();
 
-	return EXIT_SUCCESS;
+	// compute mandelbrot locally, based on subimage
+    calcMandelbrot(sub_image, size_x, size_y, start_y, num_rows, rank);
+
+	// on root, gather all subimages and write to file
+    if (rank == 0) {
+        Image final_image(num_channels * size_x * size_y);
+        MPI_Gather(sub_image.data(), sub_image_size, MPI_UINT8_T,
+                   final_image.data(), sub_image_size, MPI_UINT8_T, 0, MPI_COMM_WORLD);
+
+		// stop time
+        auto time_end = std::chrono::high_resolution_clock::now();
+        auto time_elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(time_end - time_start).count();
+        std::cout << "Mandelbrot set calculation for " << size_x << "x" << size_y << " took: " << time_elapsed << " ms." << std::endl;
+
+        constexpr int stride_bytes = 0;
+        stbi_write_png("mandelbrot_mpi.png", size_x, size_y, num_channels, final_image.data(), stride_bytes);
+    } else {
+		// if not root, send data to root (use nullptr to receive as seen in the lecture)
+        MPI_Gather(sub_image.data(), sub_image_size, MPI_UINT8_T,
+                   nullptr, 0, MPI_UINT8_T, 0, MPI_COMM_WORLD);
+    }
+
+    MPI_Finalize();
+    return EXIT_SUCCESS;
 }
