a16a4870b6e16fe9c383eac6ab086a596a639e89
LV 703078 PS Parallele Programmierung <cb761148@login.lcc3.uibk.ac.at>
2024-06-22 11:27:46 +0200

    Update build script

--- build.sh
+++ build.sh
@@ -1,7 +1,41 @@
 #!/bin/bash
 
+# Default directory
+directory="code_parallel"
+
+# Function to display help menu
+display_help() {
+    echo "Usage: $0 [OPTIONS]"
+    echo "Options:"
+    echo "  -s, --sequential    Use code_students directory"
+    echo "  -p, --parallel      Use code_parallel directory (default)"
+    echo "  -h, --help          Display this help menu"
+    exit 0
+}
+
+# Parse command-line options
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        -s|--sequential)
+            directory="code_students"
+            shift
+            ;;
+        -p|--parallel)
+            directory="code_parallel"
+            shift
+            ;;
+        -h|--help)
+            display_help
+            ;;
+        *)
+            echo "Invalid option: $1" >&2
+            exit 1
+            ;;
+    esac
+done
+
 # Move to build directory
-cd code_parallel/build
+cd "$directory/build"
 
 # load module for mpi
 module load gcc/12.2.0-gcc-8.5.0-p4pe45v openmpi/3.1.6-gcc-12.2.0-d2gmn55
