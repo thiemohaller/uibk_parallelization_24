92c607c41105524ae1fea9490d26d4b0e3b4fd23
LV 703078 PS Parallele Programmierung <cb761148@login.lcc3.uibk.ac.at>
2024-06-26 14:02:12 +0200

    Try to add gdb debug option

---
+++ job_parallel_debug.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Execute job in the partition "lva" unless you have special requirements.
+#SBATCH --partition=lva
+# Name your job to be able to identify it later
+#SBATCH --job-name test
+# Redirect output stream to this file
+#SBATCH --output=output_parallel.log
+# Maximum number of tasks (=processes) to start in total
+#SBATCH --ntasks=8
+# Maximum number of tasks (=processes) to start per node
+# SBATCH --ntasks-per-node=8
+# Enforce exclusive node allocation, do not share with other jobs
+#SBATCH --exclusive
+
+module load openmpi/3.1.6-gcc-12.2.0-d2gmn55
+
+#### NOT WORKING
+/usr/bin/time -v mpiexec -n $SLURM_NTASKS xterm -e gdb ./code_parallel/build/apps/run_full_code.parallel
---
+++ parallel_run.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+BUILD=false
+DEBUG=false
+
+# Parse arguments to get all added flags
+while [[ "$1" != "" ]]; do
+    case $1 in
+        -h|--help)
+            echo "Usage: $0 [OPTIONS]"
+            echo "Options:"
+            echo "  -h, --help     Show help menu"
+            echo "  -b, --build    Build the project before submitting"
+            echo "  -d, --debug    Submit debug job instead of regular job"
+            exit 0
+            ;;
+        -b|--build)
+            BUILD=true
+            ;;
+        -d|--debug)
+            DEBUG=true
+            ;;
+        *)
+            echo "Invalid option: $1"
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+# rebuild project (calls cmake and make)
+if [ "$BUILD" = true ]; then
+    echo "🔨 Building the project"
+    ./build.sh
+fi
+
+# submit debug version using gdb
+if [ "$DEBUG" = true ]; then
+    echo "🚀 Submitting debug job via slurm, using gdb, excellent choice"
+    sbatch job_parallel_debug.sh
+else
+    echo "🚀 Submitting regular job via slurm"
+    sbatch job_parallel.sh
+fi
+
+echo "👀 Use `watch -n 1 squeue --me` to watch your submitted jobs"
+echo "So long, and thanks for all the fish! 🐬"
